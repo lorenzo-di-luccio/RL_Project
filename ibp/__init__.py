390d458787ca574754eafaa87854ffb441c3593b
lorenzo-di-luccio <diluccio.1797569@studenti.uniroma1.it>
2024-01-26 10:53:38 +0100

    Added graphs

--- ibp/__init__.py
+++ ibp/__init__.py
@@ -3,4 +3,5 @@ from .imaginators import Imaginator_DState, Imaginator_CState
 from .ibp_agents import IBPAgent, IBPAgent_CartPole
 from .manager import Manager
 from .memory import Memory
-from .utils import read_graph_data, plot_graph
+from .utils import read_graph_data, read_list_graph_data,\
+    plot_graph, plot_list_graphs
--- ibp/utils.py
+++ ibp/utils.py
@@ -2,6 +2,7 @@ import plotly
 import plotly.express
 import plotly.graph_objects
 import polars
+from typing import List
 
 def read_graph_data(
         filename: str
@@ -9,6 +10,20 @@ def read_graph_data(
     df = polars.scan_csv(
         filename, has_header=True, separator=","
     )
+    return df.collect()
+
+def read_list_graph_data(
+        filenames: List[str]
+) -> polars.DataFrame:
+    dfs = [polars.scan_csv(
+            filename, has_header=True, separator=","
+        ).rename({
+            "Episode": f"{imag_budget}_Episode",
+            "Score": f"{imag_budget}_Score",
+            "AvgScore": f"{imag_budget}_AvgScore"
+        }) for imag_budget, filename in enumerate(filenames)
+    ]
+    df = polars.concat(dfs, how="horizontal")
     return df.collect()
 
 def plot_graph(
@@ -23,4 +38,29 @@ def plot_graph(
         y=stat,
         markers=False
     )
+    return fig
+
+def plot_list_graphs(
+        df: polars.DataFrame,
+        title: str,
+        stats: List[str]
+) -> plotly.graph_objects.Figure:
+    fig = plotly.express.line(
+        data_frame=df.to_pandas(),
+        title=title,
+        x="0_Episode",
+        y=stats,
+        labels={"0_Episode": "Episode", "value": "AvgScore"},
+        markers=False
+    )
+    new_trace_names = {
+        "0_AvgScore": "imag_budget=0",
+        "1_AvgScore": "imag_budget=1",
+        "2_AvgScore": "imag_budget=2"
+    }
+    fig.for_each_trace(lambda trace: trace.update(name=new_trace_names[trace.name]))
+    fig_params = fig.to_dict()
+    fig_params["layout"]["legend"]["title"] = None
+    fig_params["layout"]["autosize"] = False
+    fig.update_layout(**fig_params["layout"])
     return fig
